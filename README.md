e289bc6d7462d8d94838c1fa8723cc2776ce9253
Aiden <aiden@aidenwallis.co.uk>
2022-08-14 05:00:17 +0100

    wrong readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# aiden_fivem_twitch_chat (also known as `The Metagame Machine`)
+# fivem_metagame_machine (also known as `The Metagame Machine`)
 
 Really just a shitpost. Embeds Twitch chats into FiveM :)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-name "aiden_fivem_twitch_chat"
+name "fivem_metagame_machine"
 description "Embeds a Twitch chat overlay into your FiveM client. Also known as the metagame machine."
 author "Aiden"
 version "0.0.1"
