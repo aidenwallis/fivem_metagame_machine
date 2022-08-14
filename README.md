ad0025e996fdde486f1d7c6dd66f6433fd8cfca6
Aiden <aiden@aidenwallis.co.uk>
2022-08-14 04:58:45 +0100

    initial, high quality commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aiden_fivem_twitch_chat (also known as `The Metagame Machine`)
+
+Really just a shitpost. Embeds Twitch chats into FiveM :)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "aiden_fivem_twitch_chat"
+description "Embeds a Twitch chat overlay into your FiveM client. Also known as the metagame machine."
+author "Aiden"
+version "0.0.1"
+
+ui_page "index.html"
+
+files {
+    "index.html"
+}
